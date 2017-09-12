98b7d5ed7211ee5e1ac94ec6fb39018533f142c7
johnw2966 <john.walsh@ittralee.ie>
2017-09-12 15:33:55 +0100

    Welcome first attempt

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+/**
+ * Created by T00036644 on 12/09/2017.
+ */
+public class Welcome {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+        System.out.println("Hello Again");
+    }
+
+}
